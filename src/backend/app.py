171aa54ae0849f829a3432b3e421ee881ad59cee
Jesson Yo <57820472+Aphostrophy@users.noreply.github.com>
2020-11-05 21:56:08 +0700

    Flask init and experimenting with Rest API in Flask

---
+++ src/backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+@app.route('/api', methods=['GET'])
+
+def what_ismy_basedir():
+    return basedir
+
+@app.route('/api/cerita/', methods=['GET'])
+
+def hello_worpegeld():
+    cerita = request.args.get('cerita', default="", type=str)
+    data = open(os.path.join(basedir,'static/'+cerita)).read()
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
