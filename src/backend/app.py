c3e9794b744915123478b07aeb99dad1e0c64199
marcellof23 <01faria.marcello@gmail.com>
2020-11-07 03:35:50 +0700

    add stem + stopwords filter

--- src/backend/app.py
+++ src/backend/app.py
@@ -4,17 +4,18 @@ import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/api', methods=['GET'])
 
+@app.route('/api', methods=['GET'])
 def what_ismy_basedir():
     return basedir
 
-@app.route('/api/cerita/', methods=['GET'])
 
+@app.route('/api/cerita/', methods=['GET'])
 def hello_world():
     cerita = request.args.get('cerita', default="", type=str)
-    data = open(os.path.join(basedir,'static/'+cerita)).read()
+    data = open(os.path.join(basedir, 'static/'+cerita)).read()
     return data
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
