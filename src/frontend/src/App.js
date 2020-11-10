590ef77872a108569ccc08363c5014cb8fde3207
marcellof23 <01faria.marcello@gmail.com>
2020-11-11 02:17:36 +0700

    beresin dropzone

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,30 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Axios from 'axios';
-import React,{useEffect} from 'react';
-import Header from './components/Header';
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
+import React, { useEffect } from "react";
+import Header from "./components/Header";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-
-  return (
-    <div>
-    <Router>
-      <Header />
-        <Switch>
-          <Route exact path='/' component={Page1} />
-          <Route exact path='/search' component={Page2} />
-        </Switch>
-    </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Page1} />
+					<Route exact path="/search" component={Page2} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/frontend/src/components/Uploader.js
+++ src/frontend/src/components/Uploader.js
@@ -1,21 +1,49 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React from "react";
+import Dropzone from "react-dropzone-uploader";
+import "react-dropzone-uploader/dist/styles.css";
+import "./uploader.css";
 
-function Uploader() {
-  const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+const Uploader = () => {
+	const getUploadParams = ({ meta }) => {
+		return { url: "http://localhost:5000/api/upload" };
+	};
 
-  return (
-    <div className="uploader" {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
-}
-export default Uploader
+	const handleChangeStatus = ({ meta, file }, status) => {
+		console.log("CHANGEEEEEEEEEEEEEEEEEEEEEEEE");
+		console.log(status, meta, file);
+	};
+
+	const handleSubmit = (files, allFiles) => {
+		console.log("SUBMITTTTTTTTTTTTTTTTTT");
+		console.log(files);
+		console.log(files.map((f) => f.meta));
+		allFiles.forEach((f) => f.remove());
+	};
+
+	return (
+		<Dropzone
+			getUploadParams={getUploadParams}
+			onChangeStatus={handleChangeStatus}
+			onSubmit={handleSubmit}
+			styles={{
+				dropzone: { minHeight: 300, maxHeight: 250 },
+				submitButton: {
+					width: 100,
+					backgroundColor: "#e6e2cb",
+					color: "black",
+				},
+				inputLabel: {
+					color: "black",
+				},
+				inputLabelWithFiles: {
+					backgroundColor: "#e6e2cb",
+					color: "black",
+				},
+			}}
+			accept=".html"
+			inputContent="Drag Files or Click to Upload"
+		/>
+	);
+};
+
+export default Uploader;
