5d0dbf4929d8f522806784a27ccbefc8b46d8066
marcellof23 <01faria.marcello@gmail.com>
2020-11-12 21:53:49 +0700

    pull

--- src/frontend/src/components/Uploader.js
+++ src/frontend/src/components/Uploader.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import Axios from "axios";
-import React from "react";
-import Dropzone from "react-dropzone-uploader";
-import "react-dropzone-uploader/dist/styles.css";
-import "./uploader.css";
-
-const Uploader = () => {
-
-  // receives array of all files that are done uploading
-  const handleSubmit = (files) => {
-		const endpoint = "http://localhost:5000/api/upload"
-	  console.log(files.map(f => f.meta)) 
-		files.forEach(element => {
-			Axios.post(endpoint,{
-				file: element.meta
-			})
-			console.log(element.file)
-		});
-	}
-  
-  // called every time a file's `status` changes
-  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
-
-	return (
-		<>
-			<Dropzone
-				onChangeStatus={handleChangeStatus}
-				onSubmit={handleSubmit}
-				styles={{
-					dropzone: { minHeight: 300, maxHeight: 250 },
-					submitButton: {
-						width: 100,
-						backgroundColor: "#e6e2cb",
-						color: "black",
-					},
-					inputLabel: {
-						color: "black",
-					},
-					inputLabelWithFiles: {
-						backgroundColor: "#e6e2cb",
-						color: "black",
-					},
-				}}
-				accept=".html"
-				inputContent="Drag Files or Click to Upload"
-			/>
-=======
 import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
@@ -300,7 +252,6 @@ const Uploader = () => {
 					</div>
 				</div>
 			</div>
->>>>>>> 10966bb5d148b1a42459c6da1b5e0f36843e26aa
 		</>
 	);
 };
