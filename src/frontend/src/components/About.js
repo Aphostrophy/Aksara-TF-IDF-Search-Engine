af74380f64e25576d27dfa8efa3cf04261c599da
hrshfr <hrshfr1@gmail.com>
2020-11-14 13:53:23 +0700

    oper

---
+++ src/frontend/src/components/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style.css";
+
+function About(){
+    return(
+        <div className="about">
+            <div className="container-img-about">
+                <div className="img-header-about"></div>
+            </div>
+            <div className="container-profil">
+                <div className="jesson">
+                </div>
+                <div className="member">
+                    <div className="img-marcello"></div>
+                    <p><b>Jesson Gossal Yo</b> 
+                    <br/> 13519079
+                    </p>
+                </div>
+                <div className="member">
+                    <div className="img-marcello"></div>
+                    <p><b>Marcello Faria</b> 
+                    <br/> 13519086
+                    </p>
+                </div>
+                <div className="member">
+                    <div className="img-hera"></div>
+                    <p><b>Hera Shafira</b><br/>
+                    13519131
+                    </p>
+                </div>               
+            </div>
+        </div>
+    )
+}
+
+export default About
